2715ba8aadbfcfc417046e3c45126acf3bb5c420
Aleksandr Galkin <a.galkin@artspace-kzn.ru>
2021-09-22 17:59:52 +0300

    добавляет типизацию переменных и методов, правит phpdoc на опечатки

--- src/Task/StatusInterface.php
+++ src/Task/StatusInterface.php
@@ -8,13 +8,13 @@ interface StatusInterface
 {
     /**
      * Возвращает статус задачи объекта TaskStatusEnum.
-     * @return TaskStatusEnum
+     * @return TaskStatusEnum Текущий статус
      */
     public function getStatus(): TaskStatusEnum;
 
     /**
      * Задает статус задачи объекта TaskStatusEnum.
-     * @param TaskStatusEnum $status новый статус
+     * @param TaskStatusEnum $status Новый статус
      */
     public function setStatus(TaskStatusEnum $status): void;
 }
